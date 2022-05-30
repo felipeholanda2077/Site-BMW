04622a9ee919d1bcc97c39518738ac87fa69d3ef
Felipe Holanda de Freitas <79938668+felipeholanda2077@users.noreply.github.com>
2022-05-30 14:27:29 -0300

    Em Andamento

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Bmw_MVC.Models;
+
+namespace Bmw_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Formulario()
+        {
+            return View();
+        }
+
+        public IActionResult SalvarMensagem(Mensagem m)
+        {
+            return View("ConfirmarMsg", m);
+        }
+
+        
+    }
+}
---
+++ Controllers/MensagemController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Bmw_MVC.Models;
+
+namespace Bmw_MVC.Controllers
+{
+    public class MensagemController: Controller
+    {
+        
+    }
+}
